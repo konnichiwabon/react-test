b7471e2c4be108b778d5cccb3d3a03d261184171
bonteves43@gmail.com <bonteves43@gmail.com>
2025-06-24 15:23:16 +0800

    nag butang ko daghan files

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+ interface Actor {
+  name: string;
+ }
+
+interface CardProps {
+  title:string;
+  rating?: number;
+  isCool?: boolean;
+  actors?: Actor[];
+}
+
+
+
+
+const Card = ({title , rating = 0, isCool = false, actors=[]}: CardProps) => {
+  return (
+    <div>
+       <h2>{title} {rating} {isCool ? "Cool" : "Not cool "}</h2>
+      <h2>{title} {rating} {isCool ? "Cool" : "Not cool "}</h2>
+    
+    {actors.length > 0 && (
+      <ul>
+      {actors.map((actor, idx) => (
+        <li key={idx}>{actor.name}</li>
+      ))}
+        </ul>
+    )}
+    </div>
+  ) 
+}
+
+const App = () => {
+  return (
+    <div>s
+      <h2>Functional Arrow Component</h2>
+      <Card title="One Piece" rating={5} isCool={true} actors={[{name: 'ZORO'}]}/>
+      <Card title="One Piece1" />
+      <Card title="One Piece2" />
+      
+
+    </div>
+    
+  )
+  
+}
+
+export default App
